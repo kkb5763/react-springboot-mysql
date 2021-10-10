ee52e82613a41812f565fde0ad3de11a26886502
kkb5763 <52413804+kkb5763@users.noreply.github.com>
2021-10-10 23:01:32 +0900

    '1.0'

---
+++ src/main/java/kb/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package kb.springboot.repository;
+
+import kb.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
